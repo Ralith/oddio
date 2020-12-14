c5d81d41dac8ceaa253b28ed585825af8ba2b773
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-12-13 20:35:41 -0800

    Dynamic gain transformer

---
+++ src/gain.rs
@@ -0,0 +1,54 @@
+use std::sync::atomic::{AtomicU32, Ordering};
+
+use crate::{Control, Frame, Source, StridedMut};
+
+/// Scales amplitude by a dynamically-adjustable factor
+pub struct Gain<T: ?Sized> {
+    gain: AtomicU32,
+    inner: T,
+}
+
+impl<T> Gain<T> {
+    /// Apply dynamic gain to `source`
+    pub fn new(source: T) -> Self {
+        Self {
+            gain: AtomicU32::new(1.0f32.to_bits()),
+            inner: source,
+        }
+    }
+}
+
+impl<T: Source> Source for Gain<T>
+where
+    T::Frame: Frame,
+{
+    type Frame = T::Frame;
+
+    fn sample(&self, offset: f32, sample_length: f32, mut out: StridedMut<'_, Self::Frame>) {
+        self.inner.sample(offset, sample_length, out.borrow());
+        // Should we blend from the previous value?
+        let gain = f32::from_bits(self.gain.load(Ordering::Relaxed));
+        for x in &mut out {
+            *x = x.scale(gain);
+        }
+    }
+
+    fn advance(&self, dt: f32) {
+        self.inner.advance(dt);
+    }
+
+    fn remaining(&self) -> f32 {
+        self.inner.remaining()
+    }
+}
+
+impl<T> Control<Gain<T>> {
+    /// Adjust the gain
+    pub fn set_gain(&mut self, factor: f32) {
+        unsafe {
+            (*self.get())
+                .gain
+                .store(factor.to_bits(), Ordering::Relaxed);
+        }
+    }
+}
