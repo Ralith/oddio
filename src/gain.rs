f6214f03768f8cc91c47693ade4d5d6f4fba2f91
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-01-20 19:21:23 -0800

    Smooth changes in Gain

--- src/gain.rs
+++ src/gain.rs
@@ -1,10 +1,16 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::{
+    cell::Cell,
+    sync::atomic::{AtomicU32, Ordering},
+};
 
 use crate::{frame, Controlled, Filter, Frame, Seek, Signal};
 
 /// Scales amplitude by a dynamically-adjustable factor
 pub struct Gain<T: ?Sized> {
-    gain: AtomicU32,
+    shared: AtomicU32,
+    prev_gain: Cell<f32>,
+    next_gain: Cell<f32>,
+    time_since_changed: Cell<f32>,
     inner: T,
 }
 
@@ -12,9 +18,18 @@ impl<T> Gain<T> {
     /// Apply dynamic gain to `signal`
     pub fn new(signal: T) -> Self {
         Self {
-            gain: AtomicU32::new(1.0f32.to_bits()),
+            shared: AtomicU32::new(1.0f32.to_bits()),
+            prev_gain: Cell::new(1.0),
+            next_gain: Cell::new(1.0),
+            time_since_changed: Cell::new(1.0),
             inner: signal,
         }
+    }
+
+    fn gain(&self) -> f32 {
+        let diff = self.next_gain.get() - self.prev_gain.get();
+        let progress = ((self.time_since_changed.get()) / SMOOTHING_PERIOD).min(1.0);
+        self.prev_gain.get() + progress * diff
     }
 }
 
@@ -24,12 +39,19 @@ where
 {
     type Frame = T::Frame;
 
+    #[allow(clippy::float_cmp)]
     fn sample(&self, interval: f32, out: &mut [T::Frame]) {
         self.inner.sample(interval, out);
-        // TODO: Blend from the previous value
-        let gain = f32::from_bits(self.gain.load(Ordering::Relaxed));
+        let shared = f32::from_bits(self.shared.load(Ordering::Relaxed));
+        if self.next_gain.get() != shared {
+            self.prev_gain.set(self.gain());
+            self.next_gain.set(shared);
+            self.time_since_changed.set(0.0);
+        }
         for x in out {
-            *x = frame::scale(x, gain);
+            *x = frame::scale(x, self.gain());
+            self.time_since_changed
+                .set(self.time_since_changed.get() + interval);
         }
     }
 
@@ -64,11 +86,44 @@ unsafe impl<'a, T: 'a> Controlled<'a> for Gain<T> {
 impl<'a, T> GainControl<'a, T> {
     /// Get the current gain
     pub fn gain(&self) -> f32 {
-        f32::from_bits(self.0.gain.load(Ordering::Relaxed))
+        f32::from_bits(self.0.shared.load(Ordering::Relaxed))
     }
 
     /// Adjust the gain
     pub fn set_gain(&mut self, factor: f32) {
-        self.0.gain.store(factor.to_bits(), Ordering::Relaxed);
+        self.0.shared.store(factor.to_bits(), Ordering::Relaxed);
+    }
+}
+
+/// Number of seconds over which to smooth a change in gain
+const SMOOTHING_PERIOD: f32 = 0.1;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::Sample;
+
+    struct Const;
+
+    impl Signal for Const {
+        type Frame = Sample;
+
+        fn sample(&self, _: f32, out: &mut [Sample]) {
+            for x in out {
+                *x = 1.0;
+            }
+        }
+    }
+
+    #[test]
+    fn smoothing() {
+        let s = Gain::new(Const);
+        let mut buf = [0.0; 6];
+        GainControl(&s).set_gain(5.0);
+        s.sample(0.025, &mut buf);
+        assert_eq!(buf, [1.0, 2.0, 3.0, 4.0, 5.0, 5.0]);
+        s.sample(0.025, &mut buf);
+        assert_eq!(buf, [5.0; 6]);
     }
 }
