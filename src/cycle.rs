de3296bcf4e21f0a178bb0cbfb98ea8e5572c16b
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-01-23 10:45:49 -0800

    Re-define Cycle in discrete terms and simplify
    Allows correct interpolation between endpoints and reduces likelihood
    of error in the common case of looping an entire buffer, which
    previously required computing a duration of (len - 1) / rate.

--- src/cycle.rs
+++ src/cycle.rs
@@ -1,116 +1,68 @@
-use std::cell::Cell;
+use std::{cell::Cell, sync::Arc};
 
-use crate::{Filter, Frame, Seek, Signal};
+use crate::{frame, Frame, Frames, Signal};
 
-/// Loops a [`Seek`]able signal
-pub struct Cycle<T: ?Sized> {
-    duration: f32,
+/// Loops [`Frames`] end-to-end to construct a repeating signal
+pub struct Cycle<T> {
+    /// Current playback time, in samples
     cursor: Cell<f32>,
-    inner: T,
+    frames: Arc<Frames<T>>,
 }
 
 impl<T> Cycle<T> {
-    /// Loop `signal` such that it starts over after `duration` seconds
+    /// Construct cycle from `frames`
     // TODO: Crossfade
-    pub fn new(signal: T, duration: f32) -> Self {
+    pub fn new(frames: Arc<Frames<T>>) -> Self {
         Self {
-            duration,
             cursor: Cell::new(0.0),
-            inner: signal,
+            frames,
         }
     }
+
+    /// Interpolate a frame for position `sample`
+    fn interpolate(&self, sample: f32) -> T
+    where
+        T: Frame,
+    {
+        let a = sample as usize;
+        let b = (a + 1) % self.frames.len();
+        frame::lerp(&self.frames[a], &self.frames[b], sample.fract())
+    }
 }
 
-impl<T: Signal + Seek> Signal for Cycle<T>
-where
-    T::Frame: Frame,
-{
-    type Frame = T::Frame;
+impl<T: Frame + Copy> Signal for Cycle<T> {
+    type Frame = T;
 
-    fn sample(&self, interval: f32, mut out: &mut [T::Frame]) {
-        while !out.is_empty() {
-            let seconds = self.duration - self.cursor.get();
-            let samples = 1 + (seconds / interval) as usize;
-            let n = samples.min(out.len());
-            self.inner.sample(interval, &mut out[..n]);
-            self.cursor.set(self.cursor.get() + interval * n as f32);
-            if self.cursor.get() > self.duration {
-                self.seek_to(self.cursor.get());
-            }
-            out = &mut out[n..];
+    fn sample(&self, interval: f32, out: &mut [T]) {
+        let ds = interval * self.frames.rate() as f32;
+        for x in out {
+            *x = self.interpolate(self.cursor.get());
+            self.cursor
+                .set((self.cursor.get() + ds) % self.frames.len() as f32);
         }
-    }
-}
-
-impl<T> Filter for Cycle<T> {
-    type Inner = T;
-    fn inner(&self) -> &T {
-        &self.inner
-    }
-}
-
-impl<T: Seek> Seek for Cycle<T> {
-    fn seek_to(&self, t: f32) {
-        let t = t.rem_euclid(self.duration);
-        self.inner.seek_to(t);
-        self.cursor.set(t);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::cell::RefCell;
-
     use super::*;
 
-    // Logs the time of every sample
-    struct LoggedSignal {
-        times: RefCell<Vec<f32>>,
-        t: Cell<f32>,
-    }
-
-    impl Signal for LoggedSignal {
-        type Frame = f32;
-        fn sample(&self, interval: f32, out: &mut [f32]) {
-            for i in 0..out.len() {
-                self.times
-                    .borrow_mut()
-                    .push(self.t.get() + i as f32 * interval);
-            }
-            self.t.set(self.t.get() + interval * out.len() as f32);
-        }
-    }
-
-    impl Seek for LoggedSignal {
-        fn seek_to(&self, t: f32) {
-            self.t.set(t);
-        }
-    }
+    const FRAMES: &[f32] = &[1.0, 2.0, 3.0];
 
     #[test]
     fn wrap_single() {
-        let s = Cycle::new(
-            LoggedSignal {
-                t: Cell::new(0.0),
-                times: RefCell::new(Vec::new()),
-            },
-            1.25,
-        );
-        s.sample(1.0, &mut [0.0; 5]);
-        assert_eq!(s.inner.times.borrow()[..], [0.0, 1.0, 0.75, 0.5, 0.25]);
+        let s = Cycle::new(Frames::from_slice(1, FRAMES));
+        let mut buf = [0.0; 5];
+        s.sample(1.0, &mut buf);
+        assert_eq!(buf, [1.0, 2.0, 3.0, 1.0, 2.0]);
     }
 
     #[test]
     fn wrap_multi() {
-        let s = Cycle::new(
-            LoggedSignal {
-                t: Cell::new(0.0),
-                times: RefCell::new(Vec::new()),
-            },
-            2.5,
-        );
-        s.sample(1.0, &mut [0.0; 2]);
-        s.sample(1.0, &mut [0.0; 2]);
-        assert_eq!(s.inner.times.borrow()[..], [0.0, 1.0, 2.0, 0.5]);
+        let s = Cycle::new(Frames::from_slice(1, FRAMES));
+        let mut buf = [0.0; 5];
+        s.sample(1.0, &mut buf[..2]);
+        s.sample(1.0, &mut buf[2..]);
+        assert_eq!(buf, [1.0, 2.0, 3.0, 1.0, 2.0]);
     }
 }
