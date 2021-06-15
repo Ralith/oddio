93f8f745c6402c6e5d616004fcb10fdc40089ef2
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-06-14 21:26:26 -0700

    Replace wav with the more popular/flexible hound crate

--- examples/offline.rs
+++ examples/offline.rs
@@ -1,8 +1,6 @@
-use std::{fs::File, path::Path};
-
 const DURATION_SECS: u32 = 3;
 const RATE: u32 = 44100;
-const FRAME_SIZE: usize = 512;
+const BLOCK_SIZE: usize = 512;
 const SPEED: f32 = 50.0;
 
 fn main() {
@@ -25,19 +23,25 @@ fn main() {
         },
     );
 
-    let mut frames = vec![[0.0; 2]; (RATE * DURATION_SECS) as usize];
-    for chunk in frames.chunks_mut(FRAME_SIZE) {
-        oddio::run(&scene, RATE, chunk);
+    let spec = hound::WavSpec {
+        channels: 2,
+        sample_rate: RATE,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("a.wav", spec).unwrap();
+
+    for _ in 0..(RATE * DURATION_SECS / BLOCK_SIZE as u32) {
+        let mut block = [[0.0; 2]; BLOCK_SIZE];
+        oddio::run(&scene, RATE, &mut block);
+        for &frame in &block {
+            for &sample in &frame {
+                writer
+                    .write_sample((sample * i16::MAX as f32) as i16)
+                    .unwrap();
+            }
+        }
     }
 
-    let track = wav::BitDepth::Sixteen(
-        frames
-            .into_iter()
-            .flat_map(|ch| (0..2).map(move |i| ch[i]))
-            .map(|x| (x * i16::MAX as f32) as i16)
-            .collect(),
-    );
-
-    let mut out_file = File::create(Path::new("a.wav")).unwrap();
-    wav::write(wav::Header::new(1, 2, RATE, 16), track, &mut out_file).unwrap();
+    writer.finalize().unwrap();
 }
